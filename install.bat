e84d84503a45f2f0a5c088bd9e4324de304f47d6
Travis Gall <travis.gall@gmail.com>
2018-09-08 13:25:01 -0600

    install: rough draft at install

---
+++ install.bat
@@ -0,0 +1,12 @@
+mkdir C:\Users\$USER\winrice
+copy .\run.ahk C:\Program\ Files\winrice\run.ahk
+
+mkdir C:\Program Files\winrice\func
+copy .\func\* C:\Program Files\winrice\func
+
+mkdir C:\Program Files\winrice\lib
+copy .\func\* C:\Program Files\winrice\lib
+
+mkdir C:\Program Files\winrice\local
+mkdir C:\Program Files\winrice\config
+copy .\skel\* C:\Program Files\winrice\config
